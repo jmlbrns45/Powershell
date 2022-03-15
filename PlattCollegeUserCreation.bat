2c008476d137253ca260c4f2736fba29bbd14c4b
jmlbrns45 <101675716+jmlbrns45@users.noreply.github.com>
2022-03-15 18:41:52 -0400

    Add files via upload
    This is a script I wrote earlier today to automate the create of user accounts for a client at work based on what they had requested.

---
+++ PlattCollegeUserCreation.bat
@@ -0,0 +1,3 @@
+@echo off
+Powershell -File C:\Users\etsadmin\Desktop\PlattCollegeUserCreation\PlattCollegeUserCreation.ps1
+pause
---
+++ PlattCollegeUserCreation.ps1
@@ -0,0 +1,13 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -force
+
+#Gathers the information needed to create the user account in AD on DC01 for Platt College
+
+$FullName = Read-Host -Prompt "Please enter the full name of the user being created"
+$Username = Read-Host -Prompt "Please enter the username of the account you wish to create"
+$Password = Read-Host -Prompt "Please enter the password for this account" -AsSecureString
+$EmailAddress = Read-Host -Prompt "Please enter the email address for this account (should be in firstinitiallastname@plattcollege.edu format)"
+$OU = "OU=Users, OU=Colocation, DC=plattcollege, DC=local"
+
+#Creates the AD Account for the user
+
+New-ADUser -Name "$FullName" -DisplayName "$FullName" -SamAccountName $Username -AccountPassword $Password -PasswordNeverExpires $false -enabled $true -EmailAddress $EmailAddress -Path $OU -UserPrincipalName $Username@plattcollege.local
